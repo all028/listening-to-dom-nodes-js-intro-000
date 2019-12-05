b1fd08ecedf901a55571c5037b0a32cc416d16d4
al <albliu@email.com>
2019-12-05 19:17:29 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -19,18 +19,18 @@ input.addEventListener('keydown', function(e) {
 });
 
 let divs = document.querySelectorAll('div');
- 
+
 function bubble(e) {
   // remember all of those fancy DOM node properties?
   // we're making use of them to get the number
   // in each div here!
- 
+
   // if `this` is a bit confusing, don't worry —
   // for now, know that it refers to the div that
   // is triggering the current event handler.
   console.log(this.firstChild.nodeValue.trim() + ' bubbled');
 }
- 
+
 for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener('click', bubble);
 }
@@ -48,11 +48,11 @@ Try clicking on a node that's not so deeply nested -- you should still see the e
 What about capturing? In order to capture, we need to set the third argument to addEventListener to true. Let's try it out.
 
 divs = document.querySelectorAll('div');
- 
+
 function capture(e) {
   console.log(this.firstChild.nodeValue.trim() + ' captured');
 }
- 
+
 for (let i = 0; i < divs.length; i++) {
   // set the third argument to `true`!
   divs[i].addEventListener('click', capture, true);
@@ -61,10 +61,10 @@ for (let i = 0; i < divs.length; i++) {
 function bubble(e) {
   // stop! that! propagation!
   e.stopPropagation();
- 
+
   console.log(this.firstChild.nodeValue.trim() + ' bubbled');
 }
- 
+
 for (let i = 0; i < divs.length; i++) {
   divs[i].addEventListener('click', bubble);
 }
